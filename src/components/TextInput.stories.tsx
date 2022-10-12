b2d174cda223ea7bbbf3d22ffa2cb1347614116c
Bruna A Braguin <andradevbruna@gmail.com>
2022-10-12 11:37:15 -0300

    feat(checkbox): Add component & Radix/checkbox

--- src/components/TextInput.stories.tsx
+++ src/components/TextInput.stories.tsx
@@ -1,17 +1,17 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { TextInput, TextInputRootProps } from "./TextInput";
-import { Envelope } from 'phosphor-react'
+import { Envelope } from "phosphor-react";
 
 export default {
   title: "Components/TextInput",
   component: TextInput.Root,
   args: {
     children: [
-        <TextInput.Icon>
-            <Envelope/>
-        </TextInput.Icon>,
-        <TextInput.Input placeholder='Type your email address' />
-    ]
+      <TextInput.Icon>
+        <Envelope />
+      </TextInput.Icon>,
+      <TextInput.Input placeholder='Type your email address' />,
+    ],
   },
   argTypes: {
     children: {
@@ -22,4 +22,10 @@ export default {
   },
 } as Meta<TextInputRootProps>;
 
-export const Default: StoryObj<TextInputRootProps> = {}
+export const Default: StoryObj<TextInputRootProps> = {};
+
+export const WithoutIcon: StoryObj<TextInputRootProps> = {
+  args: {
+    children: <TextInput.Input placeholder='Type your email address' />,
+  },
+};
