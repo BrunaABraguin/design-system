d25eb63ddab702fb4cf1087b6f01c2e2b3fc0cfd
Bruna A Braguin <andradevbruna@gmail.com>
2022-10-14 10:41:46 -0300

    feat(prototype): add prototype figma

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,24 +1,34 @@
-import { clsx } from 'clsx';
+import { clsx } from "clsx";
 import { Slot } from "@radix-ui/react-slot";
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 
 export interface HeadingProps {
-    size?: 'sm' | 'md' | 'lg';
-    children: ReactNode;
-    asChild?: boolean;
+  size?: "sm" | "md" | "lg";
+  children: ReactNode;
+  asChild?: boolean;
+  className?: string;
 }
-export function Heading({ size = 'md', children, asChild}: HeadingProps){
-    const Comp = asChild ? Slot : 'h2';
+export function Heading({
+  size = "md",
+  children,
+  asChild,
+  className,
+}: HeadingProps) {
+  const Comp = asChild ? Slot : "h2";
 
-    return (
-      <Comp
-        className={clsx("text-gray-100 font-bold font-sans", {
+  return (
+    <Comp
+      className={clsx(
+        "text-gray-100 font-bold font-sans",
+        {
           "text-lg": size === "sm",
           "text-xl": size === "md",
           "text-2xl": size === "lg",
-        })}
-      >
-        {children}
-      </Comp>
-    );
+        },
+        className
+      )}
+    >
+      {children}
+    </Comp>
+  );
 }
